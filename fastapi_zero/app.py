ff9b87c0be8250b5e30c0d036d9804daa29f577f
Maicon <maiconplus@gmail.com>
2026-05-20 23:00:47 -0300

    schemas

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,11 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+from fastapi_zero.schemas import Message
+
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def road_root():
     return {'message': 'Olá Mundo!'}
